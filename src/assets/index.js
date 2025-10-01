fea0915fc9bbb6e45b80c0ff8703d8c42514c2c4
Voidfx1 <tochukwud882@gmail.com>
2025-10-02 00:37:51 +0100

    Fix case sensitivity for Gaming_setup and MineCraft assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,12 +3,12 @@ import Fortnite from "./Fortnite.jpg"
 import Hallow_Knight from "./Hallow_Knight.jpg"
 import Formular1 from "./Formular1.jpg"
 import Forza_Horizon5 from "./Forza_Horizon5.jpg"
-// import Minecraft from "./Minecraft.jpg"
+import Minecraft from "./MineCraft.jpg"
 import Call_Of_Duty from "./Call_Of_Duty.jpg"
 import Assasin_Creed from "./Assasin_Creed.jpg"
 import Witcher_3 from "./Witcher_3.jpg"
 import Cyberpunk_2077 from "./Cyberpunk_2077.jpg"
-import gaming_setup from "./gaming_setup.jpg"
+import gaming_setup from "./Gaming_setup.jpg"
 import God_Of_War from "./God_Of_War.jpg"
 import Red_Dead_Redemption_2 from "./Red_Dead_Redemption_2.jpg"
 
@@ -19,7 +19,7 @@ export const ASSETS = {
   Hallow_Knight,
   Formular1,
   Forza_Horizon5,
-  // Minecraft,
+  Minecraft,
   Call_Of_Duty,
   Assasin_Creed,
   Witcher_3,
